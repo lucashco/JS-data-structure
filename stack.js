057b529f6df2260da0370b6c90b723373a20bbff
Lucas Castilho <lucascastilholiveira@gmail.com>
2023-10-20 07:09:45 -0300

    feat: create stack data structure

---
+++ stack.js
@@ -0,0 +1,40 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+
+console.log(stack.isEmpty());
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.print();
+stack.size();
+stack.pop();
+stack.peek();
